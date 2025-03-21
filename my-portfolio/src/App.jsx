74341db5b6795b500fbebdde82bd0c1052791a17
Derek Naing <dereknaing01@gmail.com>
2025-03-20 23:55:07 -0700

    Setting up React 3 Fiber

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import { Canvas } from "@react-three/fiber";
+
+import Experience from './Experience';
+
+export default function App() {
+    return (
+        <Canvas>
+            <Experience />
+        </Canvas>
+    )
+}
---
+++ my-portfolio/src/Experience.jsx
@@ -0,0 +1,11 @@
+export default function Experience()
+{
+    return <>
+
+        <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+        </mesh>
+
+    </>
+}
