e4f549f61054587b47291bfa7d631489a044e6ab
Derek Naing <dereknaing01@gmail.com>
2025-04-13 20:13:28 -0700

    Added skills and icons to skills section

--- my-portfolio/src/components/ui/Skills.jsx
+++ my-portfolio/src/components/ui/Skills.jsx
@@ -1,3 +1,26 @@
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { BiLogoJavascript } from "react-icons/bi";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaPython } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { FaDartLang } from "react-icons/fa6";
+import { PiFileCSharpDuotone } from "react-icons/pi";
+import { BiLogoCPlusPlus } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
+import { FaFlutter } from "react-icons/fa6";
+import { GrMysql } from "react-icons/gr";
+import { SiMongodb } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { SiGooglecloud } from "react-icons/si";
+import { SiAmazonwebservices } from "react-icons/si";
+import { TbBrandThreejs } from "react-icons/tb";
+import { SiBlender } from "react-icons/si";
+
+
+
 export default function Skills()
 {
     return <>
@@ -5,12 +28,140 @@ export default function Skills()
         <div className="skills">
 
             <div className="skills-section">
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
-                SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS
+                
+                <div className="skills-category">
+                    <h1>LANGUAGES</h1>
+
+                    <div className="skills-list">
+
+                        <div className="skills-item">
+                        <FaHtml5 />
+                            <h4>HTML</h4>
+                        </div>
+                        <div className="skills-item">
+                        <FaCss3Alt />
+                            <h4>CSS</h4>
+                        </div>
+                        <div className="skills-item">
+                            <BiLogoJavascript />
+                            <h4>JavaScript</h4>
+                        </div>
+                        <div className="skills-item">
+                            <BiLogoTypescript />
+                            <h4>TypeScript</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaPython />
+                            <h4>Python</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaJava />
+                            <h4>Java</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaDartLang />
+                            <h4>Dart</h4>
+                        </div>
+                        <div className="skills-item">
+                            <PiFileCSharpDuotone />
+                            <h4>C#</h4>
+                        </div>
+                        <div className="skills-item">
+                            <BiLogoCPlusPlus />
+                            <h4>C++</h4>
+                        </div>
+                        
+                    </div>
+
+                    
+                </div>
+
+                <div className="skills-category">
+                    <h1>FRAMEWORKS</h1>
+
+                    <div className="skills-list">
+
+                        <div className="skills-item">
+                            <FaReact />
+                            <h4>React</h4>
+                        </div>
+                        <div className="skills-item">
+                            <SiExpress />
+                            <h4>Express</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaNode />
+                            <h4>Node.js</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaFlutter />
+                            <h4>Flutter</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="skills-category">
+                    <h1>DATABASES</h1>
+
+                    <div className="skills-list">
+
+                        <div className="skills-item">
+                            <GrMysql />
+                            <h4>MySQL</h4>
+                        </div>
+                        <div className="skills-item">
+                            <SiMongodb />
+                            <h4>MongoDB</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="skills-category">
+                    <h1>TOOLS</h1>
+
+                    <div className="skills-list">
+
+                        <div className="skills-item">
+                            <FaGithub />
+                            <h4>Git</h4>
+                        </div>
+                        <div className="skills-item">
+                            <SiGooglecloud />
+                            <h4>GCP</h4>
+                        </div>
+                        <div className="skills-item">
+                            <SiAmazonwebservices />
+                            <h4>AWS</h4>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="skills-category">
+                    <h1>3D GRAPHICS</h1>
+
+                    <div className="skills-list">
+
+                        <div className="skills-item">
+                            <TbBrandThreejs />
+                            <h4>Three.js</h4>
+                        </div>
+                        <div className="skills-item">
+                            <FaReact />
+                            <h4>R3F</h4>
+                        </div>
+                        <div className="skills-item">
+                            <SiBlender />
+                            <h4>Blender</h4>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
             
 
