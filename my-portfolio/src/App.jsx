a6f360feb7befccbedd9a0090d058ff56bea0705
Derek Naing <dereknaing01@gmail.com>
2025-04-30 17:47:28 -0700

    Added sound effect to resume download, removed some unnecessary lines from App and began prep on mobile experience and interface

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -5,23 +5,19 @@ import { Suspense } from 'react'
 import { Leva } from 'leva'
 
 import Experience from './Experience'
-import LoadingScreen from './components/ui/LoadingScreen'
 import Interface from './Interface'
+
+import MobileExperience from './MobileExperience'
+import MobileInterface from './MobileInterface'
+
+import LoadingScreen from './components/ui/LoadingScreen'
 
 export default function App() {
 
     
-
     return <>
-        <Canvas 
-            // frameloop="demand"
-            // gl={{ logarithmicDepthBuffer: true }}
-            gl={{ antialias: true }} 
-            dpr={[1, 2]}
-        >
-            <Suspense fallback={null}>
-                <Experience />
-            </Suspense>
+        <Canvas gl={{ antialias: true}} dpr={[1, 2]}>
+            <Experience />
         </Canvas>
         <Leva collapsed={true} hidden />
         <LoadingScreen />
--- my-portfolio/src/components/ui/Resume.jsx
+++ my-portfolio/src/components/ui/Resume.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react"
+
 export default function Resume()
 {
+
+    const audio = useRef()
+
+    const playClick = () =>
+    {
+        if (audio.current)
+        {
+            audio.current.volume = 0.1
+            audio.current.currentTime = 0
+            audio.current.play()
+        }
+    }
+
     return <>
+
+        <audio ref={ audio } className="click-audio" src="./audio/click.wav" preload="auto"></audio>
 
         <div className="resume">
 
@@ -14,7 +31,7 @@ export default function Resume()
                 </div>
 
                 <div className="resume-download">
-                    <a className="neon-effect" href="/images/resume/resume.pdf" download="derek-resume">Download</a>
+                    <a className="neon-effect" href="/images/resume/resume.pdf" download="derek-resume" onPointerEnter={playClick}>Download</a>
                 </div>
             
             </div>
