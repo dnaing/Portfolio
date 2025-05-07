9534a7f1fb6041d3b52f60f7f07c23de4d3aa088
Derek Naing <dereknaing01@gmail.com>
2025-05-07 15:55:15 -0700

    fixed bug where frame stopping only happened on hot reloads

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 import { Canvas } from '@react-three/fiber'
 import { Leva } from 'leva'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import useCheckMobileScreen from './stores/useCheckMobileScreen'
 import Experience from './Experience'
@@ -15,27 +15,30 @@ export default function App({ isMobile }) {
 
     // Check if the canvas is in view
     const [ isVisible, setIsVisible ] = useState(true)
-
     const canvas = useRef()
 
-    const observer = new window.IntersectionObserver(([entry]) => {
-    if (entry.isIntersecting) {
-        setIsVisible(true)
-        return
-    }
-    setIsVisible(false)
-    }, {
-    root: null,
-    threshold: 0.05, // set offset 0.1 means trigger if atleast 10% of element in viewport
-                    // set offset 0.6 means trigger if atleast 60% of element in viewport
-                    // set offset 0.05 means trigger if at least 5% of element in viewport
-    })
-
-    if (canvas.current)
+    useEffect(() => 
     {
-        observer.observe(canvas.current)
-    }
-    
+        if (canvas.current)
+        {
+            const observer = new window.IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true)
+                    return
+                }
+                setIsVisible(false)
+                }, {
+                root: null,
+                threshold: 0.05, // set offset 0.1 means trigger if atleast 10% of element in viewport
+                                // set offset 0.6 means trigger if atleast 60% of element in viewport
+                                // set offset 0.05 means trigger if at least 5% of element in viewport
+            })
+
+            observer.observe(canvas.current)
+        }
+
+    }, [])
+
     return <>
         <div ref={ canvas } className="canvas-container">
             <Canvas gl={{ antialias: true}} dpr={[1, 2]} frameloop={ isVisible ? "always" : "never" }>
