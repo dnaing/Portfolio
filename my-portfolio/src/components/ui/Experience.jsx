f795d0d6c677fb00b9c02b9196f7d7662e7b1e95
Derek Naing <dereknaing01@gmail.com>
2025-06-18 16:33:02 -0700

    updating experience section

--- my-portfolio/src/components/ui/Experience.jsx
+++ my-portfolio/src/components/ui/Experience.jsx
@@ -12,7 +12,7 @@ export default function Experience({ isMobile })
 
                     <div className="experience-card-header">
                         <h1>DataAnnotation - AI Trainer</h1> 
-                        <h2>Dec 2023 - May 2025</h2>
+                        <h2>Dec 2023 - Current</h2>
                     </div>
 
                     <hr />
@@ -28,6 +28,31 @@ export default function Experience({ isMobile })
                         </p>
                     </div>
                 </div>
+
+                <div className="experience-card">
+
+                    <div className="experience-card-header">
+                        <h1>WorkUp Externship - Software Engineer</h1> 
+                        <h2>Feb 2025 - Current</h2>
+                    </div>
+
+                    <hr />
+
+                    <div className="experience-card-body">
+                        <p>
+                            As part of this externship, I actively contribute to the continuous improvement of a 
+                            live web application by addressing reported bugs and implementing feature enhancements 
+                            on a weekly basis. My responsibilities include reviewing an existing backlog of issues 
+                            and feature requests, selecting tasks based on priority and scope, and developing 
+                            solutions that align with the project's goals. In addition to this, I take initiative 
+                            in identifying new bugs and proposing features that could improve user experience, 
+                            performance, or maintainability. These contributions are documented and presented to 
+                            the team during regular review meetings. This collaborative process has provided me 
+                            valuable insight into real-world software development workflows.
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
 
